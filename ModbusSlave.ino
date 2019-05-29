0c5dd4df0cc749815e390616dfe64555d3d353a0
Alves Leonardo <leonardo.alves@buhlergroup.com>
2019-05-29 14:05:00 -0300

    ADD simulation of a input register over Modbus

--- ModbusSlave.ino
+++ ModbusSlave.ino
@@ -3,20 +3,20 @@
 #include <LiquidCrystal.h>
 
 // modbus control constant variables
-const int ModbusNode = 1;
-const int StatusErrorPin = LED_BUILTIN;
-const int StatusLEDPin = 8;
+const short ModbusNode = 1;
+const short StatusErrorPin = LED_BUILTIN;
+const short StatusLEDPin = 8;
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const short rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // util variables declarations
 char lcdMessage[16];
-int actValueReaded;
+int actValueReaded, result;
 
-
+// configure the leds outputs
 void configureLeds()
 {
   // configure the Status Leds
@@ -27,12 +27,14 @@ void configureLeds()
   digitalWrite(StatusLEDPin, HIGH);
 }
 
+// init LCD library
 void initLCD()
 {
   lcd.begin(16, 2);
   lcd.print("Initing...");
 }
 
+// initialize the modbus server
 void initModbusServer()
 {
   // wait 1 secconds, because the user need to see the status ok led for a moment
@@ -65,12 +67,15 @@ void initModbusServer()
   initModbusServer();
 }
 
+// configure modbus register areas
 void configModbusRegisterArea()
 {
   // configure a single coil at address 0x00
   ModbusRTUServer.configureCoils(0x00, 1);
+  ModbusRTUServer.configureInputRegisters(0x00, 1);
 }
 
+// read the register of a coil and print the status on lcd
 void readModbusCoilAndPrint(int registerAddress)
 {
   // read the current value of the coil
@@ -78,11 +83,31 @@ void readModbusCoilAndPrint(int registerAddress)
 
   if (actValueReaded)
   {
-    sprintf(lcdMessage, "0x%X is TRUE", registerAddress);
+    sprintf(lcdMessage, "Coil 0x%X is TRUE", registerAddress);
   }
   else
   {
-    sprintf(lcdMessage, "0x%X is False", registerAddress);
+    sprintf(lcdMessage, "Coil 0x%X is False", registerAddress);
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print(lcdMessage);
+}
+
+// Generate a randon value for a analog input and print on lcd
+void writeModbusInputAndPrint(int registerAddress)
+{
+  // set the current value of a register
+  actValueReaded = random(360, 900);
+  result = ModbusRTUServer.inputRegisterWrite(registerAddress,actValueReaded);
+ 
+  if (result)
+  {
+     sprintf(lcdMessage, "Anlg 0x%X = %d", registerAddress, actValueReaded);
+  }
+  else
+  {
+    sprintf(lcdMessage, "Anlg 0x%X ERR", registerAddress);
   }
 
   lcd.setCursor(0, 1);
@@ -112,4 +137,5 @@ void loop()
   ModbusRTUServer.poll();
 
   readModbusCoilAndPrint(0x00);
+  writeModbusInputAndPrint(0x00);
 }
